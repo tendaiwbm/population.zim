9ea85c1d9b9f75308fa86e13b2b32b46fde251bf
tendaiwbm <tmbwanda52@gmail.com>
2024-10-25 21:12:37 +0200

    assets: API asset

--- api/populationzim/geo/views.py
+++ api/populationzim/geo/views.py
@@ -5,7 +5,7 @@ from geo.utils import queryMunyayi,wkt2Array,prepareAdminNames
 @wkt2Array
 def fetch_distribution(request):
     if distro_validator(request.GET):
-        query = f"""select {request.GET['sex']}_population_{request.GET['year']},st_astext(shape) from prelim.ward where province_name in ('Midlands','Matebeleland North')"""
+        query = f"""select {request.GET['sex']}_population_{request.GET['year']},st_astext(shape) from prelim.ward where province_name in ('Midlands','Matebeleland North','Matebeleland South')"""
         return queryMunyayi(query)
 
 @prepareAdminNames
