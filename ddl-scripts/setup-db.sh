f46db58c6833230c2b700fdd47e3e8484ceb8d0b
tendaiwbm <tmbwanda52@gmail.com>
2024-11-08 00:22:01 +0100

    mod: extract inner & outer ring on setup instead of on fetch

--- ddl-scripts/setup-db.sh
+++ ddl-scripts/setup-db.sh
@@ -3,4 +3,5 @@ psql -U tendaiwbm -d populationZim -f 02_insert.sql
 python3 03_insert.py /media/sf_shared/ade/data/province/zwe_admbnda_adm1_zimstat_ocha_20180911.shp /media/sf_shared/ade/data/district/zwe_admbnda_adm2_zimstat_ocha_20180911.shp /media/sf_shared/ade/data/ward/zwe_admbnda_adm3_zimstat_ocha_20180911.shp
 python3 03_resolve-insert.py /media/sf_shared/ade/data/district/zwe_admbnda_adm2_zimstat_ocha_20180911.shp
 psql -U tendaiwbm -d populationZim -f 03_upsert.sql
-psql -U tendaiwbm -d populationZim -f 04_manage.sql
+python3 03_upsert.py
+#psql -U tendaiwbm -d populationZim -f 04_manage.sql
