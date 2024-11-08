d2f1a8665aa75a8ef2c847ba2634201f4d427ff5
tendaiwbm <tmbwanda52@gmail.com>
2024-11-08 08:37:52 +0100

    mod: fetch geom array & remove placeholder vertices

--- api/populationzim/geo/utils.py
+++ api/populationzim/geo/utils.py
@@ -2,22 +2,12 @@ from geo.worker import request_handler
 from django.http import JsonResponse
 import math
 
-def wkt2Array(decoratee):
+def prepare_distribution(decoratee):
     def wrapper(query):
         queryResult = decoratee(query)
         geom = [pair[1] for pair in queryResult]
-        multiPoly = [string.lstrip('MULTIPOLYGON').lstrip("(((").rstrip(")))").split("),(") for string in geom]
-        polyArray = []
-        for mPoly in multiPoly: 
-            polyList = []
-            for poly in mPoly:
-                polyCoordList = []
-                for latlon in poly.split(","):
-                    polyCoordList.append([float(latlon.split(" ")[1]), float(latlon.split(" ")[0])])
-                polyList.append([polyCoordList])
-            polyArray.append(polyList)
         RESPONSE = {
-                    "coordinates": polyArray,
+                    "coordinates": [polygon if len(polygon[0]) == 1 else [[polygon[0][0],[point for point in polygon[0][1] if point != [0,0]]]] for polygon in geom],
                     "admin-level": "district",
                     "grain": "ward",
                     "values": [math.log10(pair[0]) if pair[0] > 0 else pair[0] for pair in queryResult],
@@ -25,12 +15,12 @@ def wkt2Array(decoratee):
         return JsonResponse(RESPONSE)
     return wrapper
 
-def prepareAdminNames(decoratee):
+def prepare_admin_names(decoratee):
     def wrapper(query):
         return JsonResponse({query.GET["admin"]:[admin[0] for admin in decoratee(query)]})
     return wrapper
 
 @request_handler
-def queryMunyayi(query):
+def query_munyayi(query):
     return query
 
--- api/populationzim/geo/views.py
+++ api/populationzim/geo/views.py
@@ -1,17 +1,17 @@
 from django.http import JsonResponse
 from geo.data_validation import distro_validator,admin_validator
-from geo.utils import queryMunyayi,wkt2Array,prepareAdminNames
+from geo.utils import query_munyayi,prepare_distribution,prepare_admin_names
 
-@wkt2Array
+@prepare_distribution
 def fetch_distribution(request):
     if distro_validator(request.GET):
-        query = f"""select {request.GET['sex']}_population_density_{request.GET['year']},st_astext(shape) from prelim.ward """
+        query = f"""select {request.GET['sex']}_population_density_{request.GET['year']},geom from prelim.ward """
         if "admin-names" in request.GET:
             query = " ".join([query,f"""where {request.GET['admin']}_name in""","""""".join(["(",",".join([f"'{name}'" for name in request.GET["admin-names"].split(";")]),")"])])
-        return queryMunyayi(query)
+        return query_munyayi(query)
 
-@prepareAdminNames
+@prepare_admin_names
 def fetch_admin_names(request):
     if admin_validator(request.GET):
         query = f"""SELECT {request.GET['admin']}_name FROM prelim.{request.GET['admin']}"""
-        return queryMunyayi(query)
+        return query_munyayi(query)
