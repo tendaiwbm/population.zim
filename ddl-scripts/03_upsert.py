b3d97b3be198619ed28c63a952d649c930240dbe
tendaiwbm <tmbwanda52@gmail.com>
2024-11-08 07:53:48 +0100

    mod: corrected inner/outer ring vertex order

--- ddl-scripts/03_upsert.py
+++ ddl-scripts/03_upsert.py
@@ -1,11 +1,33 @@
-import sys
+import os,sys
 sys.path.insert(0,'./vabatsiri')
 from vabatsiri.db import QExecutor as dbg
 import psycopg2
+from dotenv import load_dotenv,find_dotenv
+load_dotenv(find_dotenv())
 
 
-def extractRing(polygon):
+def tumira(query):
+    connection = psycopg2.connect(user=os.getenv('PG_USER'),
+                                  password=os.getenv('PG_PASSWORD'),
+                                  host=os.getenv('HOST'),
+                                  port=os.getenv('PORT'),
+                                  database=os.getenv('DATABASE'),
+                                  options=os.getenv('OPTIONS'))
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        return cursor.fetchall()
+
+def extract_ring(polygon):
     return [[float(point.split(" ")[1]),float(point.split(" ")[0])] for point in polygon.split(",")]
+
+def reconstruct_multi(polygons):
+    if len(polygons) == 1:
+        return [extract_ring(polygons[0])]
+    elif len(polygons) == 2:
+        innerRing = list(reversed(extract_ring(polygons[1])))
+        outerRing = extract_ring(polygons[0])
+        innerRing += [[0,0] for i in range(len(outerRing)-len(innerRing))]
+        return [outerRing,innerRing] 
 
 def wkt2Array():
     for admin in ["ward"]:
@@ -18,29 +40,15 @@ def wkt2Array():
             mPoly = mPoly.replace("MULTIPOLYGON","").strip("(((").strip(")))").split("), (")
             if len(mPoly) == 1:
                 polygons = mPoly[0].split("),(")
-                if len(polygons) == 1:
-                    multiPoly = [[extractRing(polygons[0])]]
-                elif len(polygons) == 2:
-                    innerRing = extractRing(polygons[1])
-                    outerRing = list(reversed(extractRing(polygons[0])))
-                    innerRing += [[0,0] for i in range(len(outerRing)-len(innerRing))]
-                    multiPoly = [[outerRing,innerRing]]
+                multiPoly = [reconstruct_multi(polygons)]
             elif len(mPoly) > 1:
                 multiPoly = []
                 for poly in mPoly:
                     polygons = poly.split("),(")
-                    if len(polygons) == 1:
-                        multiPoly.append([extractRing(polygons[0])])
-                    elif len(polygons) == 2:
-                        innerRing = extractRing(polygons[1])
-                        outerRing = extractRing(polygons[0])
-                        multiPoly.append([outerRing,innerRing])
-
+                    multiPoly.append([reconstruct_multi(polygons)])
             QUERY += f"""UPDATE prelim.ward
                          SET geom = ARRAY{multiPoly}
                          WHERE ward_number = {ward[idx]}
                          AND district_name = '{district[idx]}';"""
         dbg.exec(QUERY)
 
-if __name__ == "__main__":
-    wkt2Array()
