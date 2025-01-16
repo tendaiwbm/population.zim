c2a2a1b0b0aa195df70e48be72bc950d2d7b6cb6
tendaiwbm <tmbwanda52@gmail.com>
2025-01-16 08:41:53 +0100

    mod: reverted to fectching geom with wkt2Array & column shape

--- api/populationzim/geo/views.py
+++ api/populationzim/geo/views.py
@@ -4,9 +4,24 @@ from geo.utils import query_munyayi,prepare_distribution,prepare_admin_names
 @prepare_distribution
 def fetch_distribution(request):
     if distro_validator(request.GET):
-        query = f"""select {request.GET['sex']}_population_density_{request.GET['year']},geom from prelim.ward """
-        if "admin-names" in request.GET:
-            query = " ".join([query,f"""where {request.GET['admin']}_name in""","""""".join(["(",",".join([f"'{name}'" for name in request.GET["admin-names"].split(";")]),")"])])
+        
+        if request.GET['grain'] == "ward":
+            query = f"""select {request.GET['sex']}_population_density_{request.GET['year']},st_astext(shape) from prelim.{request.GET['grain']}"""
+            if "admin-names" in request.GET:
+                query = " ".join([query,f"""where {request.GET['admin']}_name in""","""""".join(["(",",".join([f"'{name}'" for name in request.GET["admin-names"].split(";")]),")"])])
+        
+        elif request.GET['grain'] in ["district","province"]:
+            query = f"""
+                        SELECT sum(b.{request.GET['sex']}_population_density_{request.GET['year']}),st_astext(shape)
+                        FROM prelim.{request.GET['grain']} AS a
+                        JOIN 
+                     """
+            subQuery = f"""(SELECT {request.GET['sex']}_population_density_{request.GET['year']},{request.GET['admin']}_name
+                           FROM prelim.ward""" 
+            if "admin-names" in request.GET:
+                subQuery = " ".join([subQuery,f"""WHERE {request.GET['admin']}_name in""","""""".join(["(",",".join([f"'{name}'" for name in request.GET["admin-names"].split(";")]),")"])])
+            
+            query = """""".join([query,subQuery, f""") as b ON a.{request.GET['admin']}_name = b.{request.GET['admin']}_name GROUP BY a.shape"""])
         return query_munyayi(query)
 
 @prepare_admin_names
