08353b722abc9c1755ad16fad9bf44daf7a181f0
tendaiwbm <tmbwanda52@gmail.com>
2025-01-17 08:31:13 +0100

    mod: post mini geometry perf_test, fetching geom not shape!

--- api/populationzim/geo/utils.py
+++ api/populationzim/geo/utils.py
@@ -2,38 +2,12 @@ from geo.worker import request_handler
 from django.http import JsonResponse
 import math
 
-def extract_ring(polygon):
-    return [[float(point.split(" ")[1]),float(point.split(" ")[0])] for point in polygon.split(",")]
-
-def reconstruct_multi(polygons):
-    multi = []
-    outerRing = extract_ring(polygons[0])
-    multi.append(outerRing)
-    if len(polygons) > 1:
-        for polygon in polygons[1:]:
-            innerRing = list(reversed(extract_ring(polygon)))
-            #innerRing += [[0,0] for i in range(len(outerRing)-len(innerRing))]
-            multi.append(innerRing) 
-    return multi
-
 def prepare_distribution(decoratee):
     def wrapper(query):
         queryResult = decoratee(query)
         geom = (pair[1] for pair in queryResult)
-        shapes = []
-        for mPoly in geom:
-            mp = mPoly.replace("MULTIPOLYGON","").strip("(((").strip(")))").split("), (")
-            if len(mp) == 1:
-                polygons = mp[0].split("),(")
-                multiPoly = [reconstruct_multi(polygons)]
-            elif len(mp) > 1:
-                multiPoly = []
-                for poly in mp:
-                    polygons = poly.split("),(")
-                    multiPoly.append([reconstruct_multi(polygons)])
-            shapes.append(multiPoly)
         RESPONSE = {
-                    "coordinates": shapes,
+                    "coordinates": [polygon if len(polygon[0]) == 1 else [[polygon[0][0],[point for point in polygon[0][1] if point != [0,0]]]] for polygon in geom],
                     "admin-level": "district",
                     "grain": "ward",
                     "values": [math.log10(pair[0]) if pair[0] > 0 else pair[0] for pair in queryResult],
--- api/populationzim/geo/views.py
+++ api/populationzim/geo/views.py
@@ -6,13 +6,13 @@ def fetch_distribution(request):
     if distro_validator(request.GET):
         
         if request.GET['grain'] == "ward":
-            query = f"""select {request.GET['sex']}_population_density_{request.GET['year']},st_astext(shape) from prelim.{request.GET['grain']}"""
+            query = f"""select {request.GET['sex']}_population_density_{request.GET['year']},geom from prelim.{request.GET['grain']}"""
             if "admin-names" in request.GET:
                 query = " ".join([query,f"""where {request.GET['admin']}_name in""","""""".join(["(",",".join([f"'{name}'" for name in request.GET["admin-names"].split(";")]),")"])])
         
         elif request.GET['grain'] in ["district","province"]:
             query = f"""
-                        SELECT sum(b.{request.GET['sex']}_population_density_{request.GET['year']}),st_astext(shape)
+                        SELECT sum(b.{request.GET['sex']}_population_density_{request.GET['year']}),geom
                         FROM prelim.{request.GET['grain']} AS a
                         JOIN 
                      """
@@ -21,7 +21,7 @@ def fetch_distribution(request):
             if "admin-names" in request.GET:
                 subQuery = " ".join([subQuery,f"""WHERE {request.GET['admin']}_name in""","""""".join(["(",",".join([f"'{name}'" for name in request.GET["admin-names"].split(";")]),")"])])
             
-            query = """""".join([query,subQuery, f""") as b ON a.{request.GET['admin']}_name = b.{request.GET['admin']}_name GROUP BY a.shape"""])
+            query = """""".join([query,subQuery, f""") as b ON a.{request.GET['admin']}_name = b.{request.GET['admin']}_name GROUP BY a.geom"""])
         return query_munyayi(query)
 
 @prepare_admin_names
