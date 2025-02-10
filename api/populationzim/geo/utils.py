22a9d122d02eb2594a837bcc37ebd70b33e7f4d9
tendaiwbm <tmbwanda52@gmail.com>
2025-02-10 07:45:42 +0100

    assets: class-based view - Distribution

--- api/populationzim/geo/utils.py
+++ api/populationzim/geo/utils.py
@@ -2,18 +2,6 @@ from geo.worker import request_handler
 from django.http import JsonResponse
 import math
 
-def prepare_distribution(decoratee):
-    def wrapper(query):
-        queryResult = decoratee(query)
-        geom = (pair[1] for pair in queryResult)
-        RESPONSE = {
-                    "coordinates": [polygon if len(polygon[0]) == 1 else [[polygon[0][0],[point for point in polygon[0][1] if point != [0,0]]]] for polygon in geom],
-                    "admin-level": "district",
-                    "grain": "ward",
-                    "values": [math.log10(pair[0]) if pair[0] > 0 else pair[0] for pair in queryResult],
-                   }
-        return JsonResponse(RESPONSE)
-    return wrapper
 
 def prepare_admin_names(decoratee):
     def wrapper(query):
--- api/populationzim/geo/views.py
+++ api/populationzim/geo/views.py
@@ -1,14 +1,28 @@
 from geo.data_validation import distro_validator,admin_validator
-from geo.utils import query_munyayi,prepare_distribution,prepare_admin_names
+from geo.utils import query_munyayi,prepare_admin_names
 from django.views import View
-from django.utils.decorators import method_decorator
-
+from django.http import StreamingHttpResponse,JsonResponse
+from math import log10
+import json
 
 class Distribution(View):
     category = "Distribution"
+    
+    def prepare_distribution(self,query):
+        queryResult = query_munyayi(query)
+        step = 100
+        for i in range(0,len(queryResult[0]),step):
+            geom = (pair[1] for pair in queryResult[i:i+step])
+            RESPONSE = {
+                        "coordinates": [polygon if len(polygon[0]) == 1 else [[polygon[0][0],[point for point in polygon[0][1] if point != [0,0]]]] for polygon in geom],
+                        "admin-level": "district",
+                        "grain": "ward",
+                        "values": [log10(pair[0]) if pair[0] > 0 else pair[0] for pair in queryResult[i+step]],
+                       }
+            yield json.dumps(RESPONSE)
 
-    @method_decorator(prepare_distribution)
     def get(self,request):
+        print(self)
         if distro_validator(request.GET):
             
             if request.GET['grain'] == "ward":
@@ -28,31 +42,7 @@ class Distribution(View):
                     subQuery = " ".join([subQuery,f"""WHERE {request.GET['admin']}_name in""","""""".join(["(",",".join([f"'{name}'" for name in request.GET["admin-names"].split(";")]),")"])])
                 
                 query = """""".join([query,subQuery, f""") as b ON a.{request.GET['admin']}_name = b.{request.GET['admin']}_name GROUP BY a.geom"""])
-            return query_munyayi(query)
-
-
-@prepare_distribution
-def fetch_distribution(request):
-    if distro_validator(request.GET):
-        
-        if request.GET['grain'] == "ward":
-            query = f"""select {request.GET['sex']}_population_density_{request.GET['year']},geom from prelim.{request.GET['grain']}"""
-            if "admin-names" in request.GET:
-                query = " ".join([query,f"""where {request.GET['admin']}_name in""","""""".join(["(",",".join([f"'{name}'" for name in request.GET["admin-names"].split(";")]),")"])])
-        
-        elif request.GET['grain'] in ["district","province"]:
-            query = f"""
-                        SELECT sum(b.{request.GET['sex']}_population_density_{request.GET['year']}),geom
-                        FROM prelim.{request.GET['grain']} AS a
-                        JOIN 
-                     """
-            subQuery = f"""(SELECT {request.GET['sex']}_population_density_{request.GET['year']},{request.GET['admin']}_name
-                           FROM prelim.ward""" 
-            if "admin-names" in request.GET:
-                subQuery = " ".join([subQuery,f"""WHERE {request.GET['admin']}_name in""","""""".join(["(",",".join([f"'{name}'" for name in request.GET["admin-names"].split(";")]),")"])])
-            
-            query = """""".join([query,subQuery, f""") as b ON a.{request.GET['admin']}_name = b.{request.GET['admin']}_name GROUP BY a.geom"""])
-        return query_munyayi(query)
+            return self.prepare_distribution(query)
 
 @prepare_admin_names
 def fetch_admin_names(request):
