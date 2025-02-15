b69bfc412c0402c2c6099c9cce5b5764dbee826b
tendaiwbm <tmbwanda52@gmail.com>
2025-02-15 09:21:55 +0100

    fix: broken Distribution JSON response

--- api/populationzim/geo/views.py
+++ api/populationzim/geo/views.py
@@ -1,25 +1,22 @@
 from geo.data_validation import distro_validator,admin_validator
 from geo.utils import query_munyayi,prepare_admin_names
 from django.views import View
-from django.http import StreamingHttpResponse,JsonResponse
+from django.http import JsonResponse
 from math import log10
-import json
 
 class Distribution(View):
     category = "Distribution"
     
     def prepare_distribution(self,query):
         queryResult = query_munyayi(query)
-        step = 100
-        for i in range(0,len(queryResult[0]),step):
-            geom = (pair[1] for pair in queryResult[i:i+step])
-            RESPONSE = {
-                        "coordinates": [polygon if len(polygon[0]) == 1 else [[polygon[0][0],[point for point in polygon[0][1] if point != [0,0]]]] for polygon in geom],
-                        "admin-level": "district",
-                        "grain": "ward",
-                        "values": [log10(pair[0]) if pair[0] > 0 else pair[0] for pair in queryResult[i+step]],
-                       }
-            yield json.dumps(RESPONSE)
+        geom = (pair[1] for pair in queryResult)
+        RESPONSE = {
+                    "coordinates": [polygon if len(polygon[0]) == 1 else [[polygon[0][0],[point for point in polygon[0][1] if point != [0,0]]]] for polygon in geom],
+                    "admin-level": "district",
+                    "grain": "ward",
+                    "values": [log10(pair[0]) if pair[0] > 0 else pair[0] for pair in queryResult],
+                   }
+        return RESPONSE
 
     def get(self,request):
         print(self)
@@ -42,7 +39,7 @@ class Distribution(View):
                     subQuery = " ".join([subQuery,f"""WHERE {request.GET['admin']}_name in""","""""".join(["(",",".join([f"'{name}'" for name in request.GET["admin-names"].split(";")]),")"])])
                 
                 query = """""".join([query,subQuery, f""") as b ON a.{request.GET['admin']}_name = b.{request.GET['admin']}_name GROUP BY a.geom"""])
-            return self.prepare_distribution(query)
+            return JsonResponse(self.prepare_distribution(query))
 
 @prepare_admin_names
 def fetch_admin_names(request):
